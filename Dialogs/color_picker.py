ae34deeff1d79d9f36abebfe99e9ab6412ad8c8e
Satanarious <satyam048niranjan@gmail.com>
2023-07-28 03:28:12 +0530

    Add color picker dialog and RGBA to 32bit conversion logic

---
+++ Dialogs/color_picker.py
@@ -0,0 +1,22 @@
+# Library Imports
+from PyQt6.QtWidgets import QColorDialog, QLineEdit
+from PyQt6.QtGui import QColor, qRgba
+
+# Relative Imports
+from Data.enums import IconPaths
+
+
+class ColorPickerButton:
+    @staticmethod
+    def openColorDialog(lineEdit: QLineEdit):
+        colorPicker: QColorDialog = QColorDialog()
+        colorPicker.setWindowIcon(IconPaths.ColorPicker)
+        color: QColor = colorPicker.getColor()
+        if color.isValid():
+            A = color.alpha()
+            R = color.red()
+            G = color.green()
+            B = color.blue()
+            print(A, R, G, B)
+            AARRGGBB = qRgba(color.red(), color.green(), color.blue(), color.alpha())
+            lineEdit.setText(str(AARRGGBB))
